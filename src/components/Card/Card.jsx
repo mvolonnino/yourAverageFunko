3120627be2bd2b1f03d0a191bbef580d4a747233
Matt Volonnino <mvolonnino12@gmail.com>
2021-02-05 14:13:06 -0500

    took out unused defined variables

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,38 +1,10 @@
 import React from "react";
 
 import "./Card.css";
-import { useDataLayerValue } from "../../context/DataLayer";
-import API from "../../utils/API";
 import noData from "../../assets/noData.jpg";
 import AddModal from "../AddModal/AddModal";
 
 const Card = ({ data, genre }) => {
-  const [{ user, authToken, reGetUserFunkos }, dispatch] = useDataLayerValue();
-
-  const handleAddFunkoPop = (e) => {
-    e.preventDefault();
-    const { name, number, image } = data;
-    const { uid } = user;
-
-    if (!reGetUserFunkos) {
-      dispatch({
-        type: "REGET_USER_FUNKOS",
-        reGetUserFunkos: true,
-      });
-    }
-
-    const funko = {
-      genre: genre,
-      name: name,
-      number: number,
-      image: image,
-      user: true,
-    };
-    API.addFunkoPopTooUser(uid, funko, authToken)
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
-  };
-
   return (
     <div className="card funkoCard">
       <div className="card-img-top text-center mt-3">
